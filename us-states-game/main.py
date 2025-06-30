4a350b28842a22fbe474f3ce59ea83065034cd98
SyedSalahuddin1 <humbleakh21@gmail.com>
2025-06-30 23:00:10 +0530

    Add files via upload

---
+++ us-states-game/main.py
@@ -0,0 +1,50 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+states_list = data["state"].to_list()
+location_x = data["x"].to_list()
+location_y = data["y"].to_list()
+
+# print(states_list)
+# print(location_x)
+# print(location_y)
+
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+#
+#
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+
+guessed_states = []
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another states name?").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in states_list:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        # print(missing_states)
+        break
+    if answer_state in states_list and answer_state not in guessed_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        # t.write(state_data.state.item())
+        t.write(answer_state)
+
+# screen.exitonclick()
+
