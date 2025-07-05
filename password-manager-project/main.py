7ef6030e89549ea62c3bfd66967f27a0bf92599b
SyedSalahuddin1 <humbleakh21@gmail.com>
2025-07-05 13:09:49 +0530

    Add files via upload

--- password-manager-project/main.py
+++ password-manager-project/main.py
@@ -1,7 +1,45 @@
 from tkinter import *
 from tkinter import messagebox
+from random import randint, choice, shuffle
+import pyperclip
+
 YELLOW = "#f7f5dd"
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# Password Generator Project
+
+# password_list = []
+
+# for char in range(nr_letters):
+#     password_list.append(random.choice(letters))
+
+
+# for char in range(nr_symbols):
+#     password_list += random.choice(symbols)
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_numbers + password_symbols
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    print(f"Your password is: {password}")
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
+
+# for char in range(nr_numbers):
+#     password_list += random.choice(numbers)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -15,7 +53,7 @@ def save():
         messagebox.showinfo(title="OOPS", message="Please make sure you haven't left any fields empty.")
     else:
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}"
-                           f"\nPassword: {password} \nIs it ok to save?")
+                                                              f"\nPassword: {password} \nIs it ok to save?")
     if is_ok:
         with open("data.txt", "a") as data_file:
             data_file.write(f"{website} | {email} | {password}\n")
@@ -55,7 +93,8 @@ email_entry.insert(0, "salah@gmail.com")
 password_entry = Entry(width=18)
 password_entry.grid(row=3, column=1, sticky="w", columnspan=2)
 
-gen_pass = Button(text="Generate Password")
+gen_pass = Button(text="Generate Password", command=generate_password)
+
 gen_pass.grid(row=3, column=2, sticky="w")
 
 add_butt = Button(text="ADD", width=36, command=save)
@@ -64,6 +103,5 @@ add_butt.grid(row=4, column=1, columnspan=2, sticky="w")
 window.grid_columnconfigure(0, weight=0)
 window.grid_columnconfigure(1, weight=1)
 window.grid_columnconfigure(2, weight=0)
-
 
 window.mainloop()
